370537738f23121c41c9f27f67aba84312659fda
gricn <gricn666@gmail.com>
2021-05-03 03:12:17 +0800

    go to sleep

--- proxy/proxy.js
+++ proxy/proxy.js
@@ -13,15 +13,13 @@ const client = new Client({
     username: process.env.ELASTIC_USERNAME,
     password: process.env.ELASTIC_PASSWORD
   }
-
 })
 
 app.get('/api/search', async (req, res) => {
   try {
-    let body = {}
     let searchContent = req.query.q || ''
     async function run() {
-      const {body} = await client.search({
+      const { body } = await client.search({
         index: 'test',
         body: {
           query: {
@@ -31,20 +29,20 @@ app.get('/api/search', async (req, res) => {
           }
         }
       })
-      console.log(body.hits.hits)
+   
       return body
     }
     run()
-    .then(body=>{
-      res.set({
+      .then(body => {
+        res.set({
           "Access-Control-Allow-Origin": "http://localhost:3000",
           "Access-Control-Allow-Methods": "GET, PUT, PATCH, POST, DELETE",
           "Access-Control-Allow-Headers": "Content-Type, x-requested-with"
         })
-      res.send(body.hits.hits)
-    })
-    
-    
+        res.send(body)
+      })
+
+
   } catch (e) {
     // res.sendStatus(500)
     console.log(e)
